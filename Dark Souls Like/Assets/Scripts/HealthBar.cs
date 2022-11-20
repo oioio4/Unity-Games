113f472d448ed3f8948a5e8d27dee66a4928a98f
oioio4 <nathanochan@gmail.com>
2022-11-20 01:06:55 -0500

    Fixed healthbar

--- Dark Souls Like/Assets/Scripts/HealthBar.cs
+++ Dark Souls Like/Assets/Scripts/HealthBar.cs
@@ -9,13 +9,13 @@ namespace NC
     {
         private Slider slider;
 
-        private void Start() {
+        private void Awake() {
             slider = GetComponent<Slider>();
         }
 
         public void SetMaxHealth(int maxHealth) {
             slider.maxValue = maxHealth;
-            slider.value = maxHealth;
+            slider.value = maxHealth; 
         }
 
         public void SetCurrentHealth(int currentHealth) {
