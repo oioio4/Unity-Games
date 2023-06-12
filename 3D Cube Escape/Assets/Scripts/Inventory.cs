99a215d07e1284d63c44fca1d0b0e5cfeee6955f
oioio4 <nathanochan@gmail.com>
2023-06-11 20:55:14 -0500

    Updated Inventory Popup System

--- 3D Cube Escape/Assets/Scripts/Inventory.cs
+++ 3D Cube Escape/Assets/Scripts/Inventory.cs
@@ -15,6 +15,9 @@ public class Inventory : MonoBehaviour
     public Color defaultColor;
     public Color selectedColor;
 
+    // item popup
+    public GameObject itemPopup;
+
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
             for (int i = 0; i < isSelected.Length; i++) {
@@ -22,49 +25,49 @@ public class Inventory : MonoBehaviour
                     isSelected[i] = false;
             }
             isSelected[0] = !isSelected[0];
+            SwapSelected();
         } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 1)
                     isSelected[i] = false;
             }
             isSelected[1] = !isSelected[1];
+            SwapSelected();
         } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 2)
                     isSelected[i] = false;
             }
             isSelected[2] = !isSelected[2];         
+            SwapSelected();
         } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 3)
                     isSelected[i] = false;
             }
-            isSelected[3] = !isSelected[3];          
+            isSelected[3] = !isSelected[3];  
+            SwapSelected();        
         } else if (Input.GetKeyDown(KeyCode.Alpha5)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 4)
                     isSelected[i] = false;
             }
-            isSelected[4] = !isSelected[4];        
+            isSelected[4] = !isSelected[4];
+            SwapSelected();        
         } else if (Input.GetKeyDown(KeyCode.Alpha6)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 5)
                     isSelected[i] = false;
             }
-            isSelected[5] = !isSelected[5];       
+            isSelected[5] = !isSelected[5]; 
+            SwapSelected();      
         } else if (Input.GetKeyDown(KeyCode.Alpha7)) {
             for (int i = 0; i < isSelected.Length; i++) {
                 if (i != 6)
                     isSelected[i] = false;
             }
-            isSelected[6] = !isSelected[6];         
-        }
-
-        SwapSelected();
-        for (int i = 0; i < isSelected.Length; i++) {
-            if (isSelected[i]) {
-                curSelected = slots[i];
-            }
+            isSelected[6] = !isSelected[6];
+            SwapSelected();         
         }
     }
 
@@ -75,6 +78,23 @@ public class Inventory : MonoBehaviour
                 slots[i].GetComponent<Image>().color = selectedColor;
             } else {
                 slots[i].GetComponent<Image>().color = defaultColor;
+            }
+        }
+
+        for (int i = 0; i < isSelected.Length; i++) {
+            if (isSelected[i]) {
+                // set the current selected slot
+                if (slots[i].transform.childCount > 0) {
+                    curSelected = slots[i].transform.GetChild(0).gameObject;
+                }
+
+                // instantiate popup w/ item name
+                if (curSelected != null) {
+                    GameObject popup = Instantiate(itemPopup, curSelected.transform.position + new Vector3(0f, 40f, 0f), Quaternion.identity);
+                    popup.transform.SetParent(curSelected.transform, true);
+                    popup.GetComponentInChildren<Text>().text = curSelected.name.Substring(0, curSelected.name.Length - 7);
+                    //Destroy(popup, 3f);                
+                }
             }
         }
     }
