3557d683f413e6d9d69f32ec5fb4a29d2cee5fd6
oioio4 <nathanochan@gmail.com>
2023-07-03 22:04:19 -0500

    TextEdits & Slight adjustments

---
+++ 3D Cube Escape/Assets/Scripts/InteractableScripts/FaucetInteractable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaucetInteractable : Interactable
+{
+    public override void InteractionEvent() {
+        GameObject item = inventory.curSelected;
+        if (item != null) {
+            if (item.name.Substring(0, item.name.Length - 7) == "log") {
+                // gets rid of the item from the inventory
+                inventory.DestroyCur();
+
+                gm.logFlag = true;
+                interactAction.Invoke();
+                active = false;
+            }
+        }
+    }
+}
